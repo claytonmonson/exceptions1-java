a8a8826ef6932c883ae9f054700a2606f8bb2aad
Clayton <clayton_monson@hotmail.com>
2022-07-21 18:01:16 -0300

    Good commit

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Exercícios do curso de Java do Professor Nélio Alves \
 Tratamento de exceções - Seção 15 \
 Item 166 - Primeira solução - Muito ruim \
 Item 167 - Segunda solução - Ruim \
-Item 168 - Terceira solução - Boa \
+Item 168 - Terceira solução - Boa 
 
 [Java COMPLETO Programação Orientada a Objetos +Projetos](https://www.udemy.com/course/java-curso-completo/)
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
 
 	private Integer roomNumber;
@@ -13,6 +15,10 @@ public class Reservation {
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
 	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			throw new DomainException("Error in reservation: Reservation dates for update must be future dates");
+		}
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -41,21 +47,20 @@ public class Reservation {
 
 	}
 
-	public String updateDate(Date checkIn, Date checkOut) {
+	public void updateDate(Date checkIn, Date checkOut) {
 
 		Date now = new Date();
 
 		if (checkIn.before(now) || checkOut.before(now)) {
-			return "Error in reservation: Reservation dates for update must be future dates";
+			throw new DomainException("Error in reservation: Reservation dates for update must be future dates");
 		}
 		if (!checkOut.after(checkIn)) {
-			return "Error in reservation: check-out date must be after check-in date";
+			throw new DomainException("Error in reservation: check-out date must be after check-in date");
 		}
 
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
 
-		return null;
 	}
 
 	@Override
